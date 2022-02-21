3635f4dac095188634286dbd9809249635d054f5
Seiya Sasaki <sirukufarios@gmail.com>
2022-02-22 06:57:55 +0900

    add views

--- helloworldapp/urls.py
+++ helloworldapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import hellofunction
+from .views import DiaryList, DiaryCreate, DiaryDetail
 
 
 urlpatterns = [
-    path('', hellofunction)
+    path('', DiaryList.as_view(), name='list'),
+    path('create/', DiaryCreate.as_view(), name='create'),
+    path('detail/<int:pk>', DiaryDetail.as_view(), name='detail'),
 ]
--- ml_sample/settings.py
+++ ml_sample/settings.py
@@ -136,7 +136,8 @@ AUTH_PASSWORD_VALIDATORS = [
 #################################
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 TIME_ZONE = 'Asia/Tokyo'
 USE_I18N = True
 USE_TZ = True
@@ -147,7 +148,7 @@ USE_TZ = True
 ######################################################
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 # heroku settings
