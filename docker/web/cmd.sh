0512d7ef17aca8d28d6b18d588e07d1d7fde7940
siruku6 <sirukufarios@gmail.com>
2022-03-05 23:40:49 +0900

    Feature: add sklearn, numpy and joblib

--- docker/web/cmd.sh
+++ docker/web/cmd.sh
@@ -1,8 +1,4 @@
 #!/bin/ash
 # I use /bin/ash because in alpine container, we can't use bin/bash.
-
-pipenv lock -r --dev > requirements.txt
-pip install -r requirements.txt
-
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
