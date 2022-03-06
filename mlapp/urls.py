66544517d2ee58be2d795579278eaa98a7211219
siruku6 <sirukufarios@gmail.com>
2022-03-07 07:05:33 +0900

    Feature: enable to delete customer

--- mlapp/urls.py
+++ mlapp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('input_form', views.input_form, name='mlapp/input_form'),
     path('result/', views.result, name='result'),
-    path('history/', views.CustomerList.as_view(), name='history'),
+    path('history/', views.history, name='history'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
 ]
